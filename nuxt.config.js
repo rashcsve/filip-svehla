18710bb24c94172218efb61c799b0d7991b743ef
Svetlana Rashchupkina <svetlana.rashchupkina@gmail.com>
2021-03-02 20:55:54 +0100

    :art: Fix SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,24 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Filip Švehla',
+    title: 'Filip Švehla | Czech Artist',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Czech Artist' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Young Czech Artist, Paintings and Installations',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Young Czech Artist',
+      },
+      { hid: 'og:image', property: 'og:image', content: '/background.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
